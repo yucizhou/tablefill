f3613df5f1be839b4a18904fe4f12ce9f54c67f3
Yuci Zhou <yucizhou@mit.edu>
2020-06-03 16:36:19 -0400

    Update __init__.py

--- tablefill/__init__.py
+++ tablefill/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-__author__  = """Mauricio Cáceres Bravo"""
-__email__   = 'caceres@nber.org'
+__author__  = """Yuci Zhou, based on work by Mauricio Cáceres Bravo"""
 __version__ = '0.9.6'
 
 from .tablefill import tablefill
